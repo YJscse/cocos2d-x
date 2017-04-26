bf6c93a9311e7009f0ac83f2f2fe72090efb5046
KYM2-02-PC\KYM2-02 <anrhd3@naver.com>
2017-04-26 19:36:18 +0900

    01.Basic-TileMapEx4까지 추가 05.Game-TileRun 하는중..

---
+++ 05.Game/01.ProtoType/05.TileRun/Classes/HelloWorldScene.cpp
@@ -0,0 +1,76 @@
+﻿#include "HelloWorldScene.h"
+
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    auto scene = Scene::create();
+    auto layer = HelloWorld::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+
+bool HelloWorld::init()
+{
+
+    if ( !LayerColor::initWithColor(Color4B(255, 255,255,255) ))
+    {
+        return false;
+    }
+	
+	winSize = Director::getInstance()->getWinSize();
+
+	tmap = TMXTiledMap::create("TileMaps/TileRunMap.tmx");
+	background = tmap->getLayer("Background");
+	Ground = tmap->getLayer("Ground");
+	Ground->setVisible(false);
+	this->addChild(tmap, 0, 11);
+
+	Sister = Sprite::create("Images/grossinis_sister1.png");
+	Sister->setPosition(120, 140);
+	Sister->setScale(0.7f);
+
+	this->addChild(Sister);
+	this->schedule(schedule_selector(HelloWorld::moveBackground), 0.01f);
+
+	xValue = -2;
+
+    return true;
+}
+
+//void HelloWorld::setPlayerPosition(Vec2 position)
+//{
+//	Vec2 tileCoord = this->(position);
+//
+//	// 현재 위치의 Tile GID 구하기
+//	int tileGid = this->Ground->getTileGIDAt(tileCoord);
+//
+//	if (tileGid)
+//	{
+//		Value properties = tmap->getPropertiesForGID(tileGid);
+//
+//		if (!properties.isNull())
+//		{
+//			std::string cliff = properties.asValueMap()["Cliff"].asString();
+//			if (cliff == "YES")
+//			{
+//				log("낭떠러지 입니다...");
+//				return;
+//			}
+//		}
+//	}
+//
+//}
+
+void HelloWorld::moveBackground(float f)
+{
+	if (tmap->getPosition().x < -800)
+	{
+		tmap->setPosition(Vec2(0, 0));
+	}
+
+	tmap->setPosition(Vec2(tmap->getPosition().x + xValue, tmap->getPosition().y));
+
+}
---
+++ 05.Game/01.ProtoType/05.TileRun/Classes/HelloWorldScene.h
@@ -0,0 +1,37 @@
+﻿#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)             // 한글나오게 하는 헤더
+#pragma execution_character_set("utf-8")
+#endif
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::LayerColor
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+        
+    CREATE_FUNC(HelloWorld);
+
+	cocos2d::Size         winSize;
+	cocos2d::Sprite*      Sister;
+	cocos2d::TMXTiledMap* tmap;
+	cocos2d::TMXLayer*    background;
+	cocos2d::TMXLayer*    Ground;
+
+	/*virtual void onEnter();
+	virtual void onExit();
+	virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+	virtual void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);*/
+
+	//void setViewpointCenter(cocos2d::Vec2 position);
+	void setPlayerPosition(cocos2d::Vec2 position);
+	void moveBackground(float f);
+	float xValue;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
